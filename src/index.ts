626204066736011c8e05b0d0a91a7b33d47c0116
Vladislava <franckevva@gmail.com>
2021-09-14 18:25:21 +0300

    return static files

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import connect from "./db/connect";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
-const allowedOrigins = ["http://localhost:4200"];
+const allowedOrigins = ["http://localhost:8081"];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
@@ -17,10 +17,17 @@ const app = express();
 app.use(cors(options));
 app.use(json());
 
+const path = __dirname + "/views/";
+app.use(express.static(path));
+
+app.get("/", function (req, res) {
+  res.sendFile(path + "index.html");
+});
+
 app.use(sceneRouter);
 
 connect();
 
 app.listen(port, host, () => {
-  console.log("server is listening on port " + host + port);
+  console.log(`server is listening on port: ${host}//:${port}`);
 });
